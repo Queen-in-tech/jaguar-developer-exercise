fd9994b5ce37cee743112af5705181b716c47e24
queensheba <oluwadarequeensheba@example.com>
2024-04-28 15:55:47 +0100

    solution

--- exercise.rb
+++ exercise.rb
@@ -5,7 +5,29 @@ class Exercise
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
     # TODO: Implement this method
+    
+    #step 1: Split string into an array of words
+    words = str.split(/\b/)
+
+    #step 2: iterate over word in word array
+    words.each_with_index do |word, index|
+
+      #step 3: check if length is greater than 4
+      if word.length > 4
+        # step 4: if condition is true check, if word is capitalized and replace word as needed
+        if word == word.capitalize
+          words[index] = "Marklar"
+        else 
+          words[index] = "marklar"
+        end
+      end
+    end
+
+    #step 5: return new string with changed words
+    return words.join("")
+
   end
+
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
   # the "nth" term in the sequence
@@ -13,6 +35,23 @@ class Exercise
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
     # TODO: Implement this method
+
+    return 0 if nth == 0
+    sequence = [0, 1]
+    even_sequence = []
+
+    (2..nth).each do |i|
+      sequence << sequence[i - 1] + sequence[i - 2]
+    end
+
+    sequence.each do |i|
+      if i % 2 == 0
+        even_sequence << i
+      end
+    end
+
+      return even_sequence.sum
   end
 
+  puts self.even_fibonacci(5)
 end
